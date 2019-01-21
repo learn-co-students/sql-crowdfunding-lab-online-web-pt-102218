fce89608302e79c0235fb174644ac22319792cd7
Jane Venter <janeanneventer@gmail.com>
2019-01-21 03:58:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO projects VALUES (1, "voice_recorder", "music", 500000, "February 1st, 2019", "March 31st, 2019");
+INSERT INTO projects VALUES (2, "lawn_mower", "landscape", 400000, "March 15th, 2019", "June 1st, 2019" );
+INSERT INTO projects VALUES (3, "data_maker", "computers", 100000, "January 31st, 2019", "April 2nd, 2019");
+INSERT INTO projectS VALUES (4, "dress_chic", "fashion", 200000, "January 3rd, 2019", "January 19th, 2019");
+INSERT INTO projects VALUES (5,"enviro_breath", "environment", 300000, "January 16th, 2019", "April 15th, 2019");
+INSERT INTO projects VALUES (6, "happy_sun", "living", 100000, "June 1st, 2019", "July 31st, 2019");
+INSERT INTO projects VALUES (7, "quilter", "outreach", 70000, "January 21st, 2019, Febuary 1st, 2019");
+INSERT INTO projects VALUES (8, "globe_start", "education", 500000, "Febuary 15th, 2019", "April 1st, 2019");
+INSERT INTO projects VALUES (9, "sticky_icky", "play", 300000, "Febuary 21st, 2019", "May 15th, 2019");
+INSERT INTO projects VALUES (10, "music_box", "music", 600000, "March 2nd, 2019", "June 14th, 2019");
+
+#INSERT INTO users VALUES (1, )
